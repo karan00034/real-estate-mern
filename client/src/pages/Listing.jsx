8001ef1af40f491be37125758f61042dec562bc0
Karan Bhardwaj <91610499+karan00034@users.noreply.github.com>
2024-08-02 16:38:59 +0530

    Update Listing.jsx

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -16,6 +16,9 @@ import {
 } from 'react-icons/fa';
 import Contact from '../components/Contact';
 
+import { BASE_URL } from '../config';
+
+
 // https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
 export default function Listing() {
@@ -32,7 +35,7 @@ export default function Listing() {
     const fetchListing = async () => {
       try {
         setLoading(true);
-        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const res = await fetch(`${BASE_URL}/api/listing/get/${params.listingId}`);
         const data = await res.json();
         if (data.success === false) {
           setError(true);
