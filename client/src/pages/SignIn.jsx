f46539d5f29ff01985969e9cb77ffed6c9c06cdb
Karan Bhardwaj <91610499+karan00034@users.noreply.github.com>
2024-08-02 18:32:32 +0530

    Update SignIn.jsx

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -34,7 +34,7 @@ export default function SignIn() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      console.log(data);
+      console.log("Json data : ",data);
       const text = await res.text(); // Read the response as text first
       console.log('Raw response:', text); // Log the text to inspect its content
 
